85d877f64d746252a44029def5e5502ba50e1c35
sion <zxd51549395@163.com>
2020-12-28 17:36:36 +0800

    TOT

---
+++ lib/common/BaseUrl.dart
@@ -0,0 +1,5 @@
+class BaseUrl {
+  static const BASE_URL = "https://gank.io/api";
+
+  static const HOME_BANNER = "/v2/banners";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
+  // ignore: slash_for_doc_comments
   @override
   Widget build(BuildContext context) {
-    // final wordPair = new WordPair.random();
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'StartUp Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: new RandomWords(),
-    );
-  }
-}
 
-class RandomWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => RandomWordState();
-}
-
-class RandomWordState extends State<RandomWords> {
-  final wordPair = new WordPair.random();
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final _saved = new Set<WordPair>();
-
-  @override
-  Widget build(BuildContext context) {
-    // return new Text(wordPair.asPascalCase);
-    void _pushSaved() {
-      Navigator.of(context).push(
-        new MaterialPageRoute(
-          builder: (context) {
-            final tiles = _saved.map((wordPair) {
-              return new ListTile(
-                title: new Text(
-                  wordPair.asPascalCase,
-                  style: _biggerFont,
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    'Oeschinen Lake Campground',
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              );
-            });
-            final divided = ListTile.divideTiles(
-              tiles: tiles,
-              context: context,
-            ).toList();
-            return new Scaffold(
-              appBar: new AppBar(
-                title: new Text("Saved Suggestions"),
+                new Text(
+                  'Kandersteg, Switzerland',
+                  style: new TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          new Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          new Text('41'),
+        ],
+      ),
+    );
+
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+      return new Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Icon(
+            icon,
+            color: color,
+          ),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
               ),
-              body: new ListView(
-                children: divided,
-              ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       );
     }
 
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('StartUp Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
         ],
       ),
-      body: _buildSuggestions(),
     );
-  }
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
+    Widget textSection = new Container(
+      padding: new EdgeInsets.all(32.0),
+      child: new Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
     );
-  }
 
-  Widget _buildRow(WordPair wordPair) {
-    final _alreadySaved = _saved.contains(wordPair);
-    return new ListTile(
-      title: new Text(
-        wordPair.asPascalCase,
-        style: _biggerFont,
+    return new MaterialApp(
+      title: 'TOT',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      trailing: new Icon(
-        _alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: _alreadySaved ? Colors.red : null,
+      home: new Scaffold(
+        body: new ListView(
+          children: <Widget>[
+            new Image.asset(
+              'images/lake.jpg',
+              width: 600.0,
+              height: 240.0,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
       ),
-      onTap: () {
-        setState(() {
-          if (_alreadySaved) {
-            _saved.remove(wordPair);
-          } else {
-            _saved.add(wordPair);
-          }
-        });
-      },
     );
   }
 }
