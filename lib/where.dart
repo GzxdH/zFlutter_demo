8a5e736f9374aa3f57220b9663534b6876f703ad
sion <zxd51549395@163.com>
2021-01-25 17:43:47 +0800

    该咋说呢

--- lib/where.dart
+++ lib/where.dart
@@ -13,17 +13,80 @@ class WhereApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Scaffold(
-        body: new ListView(
-          children: <Widget>[
-            new Image.asset(
-              'images/lake.jpg',
-              width: 600.0,
-              height: 240.0,
-              fit: BoxFit.cover,
-            ),
-          ],
+      home: WherePage(),
+    );
+  }
+}
+
+class WherePage extends StatefulWidget {
+  WherePage_ createState() => WherePage_();
+}
+
+// ignore: camel_case_types
+class WherePage_ extends State<WherePage> {
+  Widget horizontalSection = new Center(
+    child: new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        SizedBox(
+          width: 10.0,
         ),
+        Expanded(
+          flex: 1,
+          child: new Image.asset(
+            'images/depositphot1.jpg',
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Expanded(
+          flex: 1,
+          child: new Image.asset(
+            'images/depositphot2.jpg',
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Expanded(
+          flex: 1,
+          child: new Image.asset(
+            'images/depositphot3.jpg',
+            width: 100.0,
+            height: 100.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+      ],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ListView(
+        children: <Widget>[
+          new Image.asset(
+            'images/lake.jpg',
+            width: 600.0,
+            height: 240.0,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          horizontalSection,
+        ],
       ),
     );
   }
